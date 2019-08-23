1f696108db4482ff06cb7a36907fc92f452a3804
gyarasu <dev.yoshitsugu@gmail.com>
2019-08-23 19:38:18 +0900

    feat: add sample endpoint

---
+++ functions/src/routes/sample/getBookmarks.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+import { firebaseAuth } from "../../middleware/firebaseAuth";
+import { genFunctionName } from "../../utils/functions";
+import { IRoutes } from "../../interfaces";
+
+const router = Router();
+
+// If this endpoint requires firebase authentication, add firebase middleware
+router.use("/", firebaseAuth);
+
+router.get("/", (req: Request, res: Response) => {
+  res.status(200).json([
+    {
+      id: 123,
+      name: 'My favorite things',
+    },
+    {
+      id: 345,
+      name: 'Someday My Prince Will Come',
+    }
+  ]);
+});
+
+export const getBookmarks: IRoutes = {
+  name: genFunctionName(__dirname, __filename),
+  router
+};
--- functions/src/routes/sample/index.ts
+++ functions/src/routes/sample/index.ts
@@ -1,4 +1,5 @@
 import { userinfo } from "./userinfo";
+import { getBookmarks } from './getBookmarks';
 import { IRoutes } from "../../interfaces";
 
-export const sample: IRoutes[] = [userinfo];
+export const sample: IRoutes[] = [userinfo, getBookmarks];
